3e026fb7f5c28866aa7e2cac52657c542b3b5025
James Doe <james@gmail.com>
2019-12-26 17:05:06 +0400

    slider for cover letter templates fixed

--- resources/views/cover-letter/templates.blade.php
+++ resources/views/cover-letter/templates.blade.php
@@ -54,34 +54,36 @@
 	<script src="{{asset('assets/libs/js/owl.carousel.min.js')}}"></script>
 {{--	<script src="{{asset('js/carousel.js')}}"></script>--}}
 	<script>
-        let userCoverLetter =  @json(auth()->user()->coverLetter) ;
+        $(document).ready(function () {
+            let userCoverLetter =  @json(auth()->user()->coverLetter) ;
 
-		$('#owl6').owlCarousel({
-			items: {{ $templates->count() }},
-			loop: true,
-			margin: 10,
-			dots: false,
-			center: true,
-            startPosition: userCoverLetter ? userCoverLetter.template_id - 1 : 3,
-			nav: true,
-			navText: [
-				'<img src="../assets/images/left-arrow.png">',
-				'<img src="../assets/images/right-arrow.png">'
-			],
-			navContainer: '.main-content .custom-nav',
-			mouseDrag: false,
-			responsiveClass: true,
-			responsive: {
-				0: {
-					items: 1,
-				},
-				600: {
-					items: 2,
-				},
-				1000: {
-					items: 3,
-				}
-			}
-		});
+            $('#owl6').owlCarousel({
+                items: {{ $templates->count() }},
+                loop: true,
+                margin: 10,
+                dots: false,
+                center: true,
+                startPosition: userCoverLetter ? userCoverLetter.template_id - 1 : 3,
+                nav: true,
+                navText: [
+                    '<img src="../assets/images/left-arrow.png">',
+                    '<img src="../assets/images/right-arrow.png">'
+                ],
+                navContainer: '.main-content .custom-nav',
+                mouseDrag: false,
+                responsiveClass: true,
+                responsive: {
+                    0: {
+                        items: 1,
+                    },
+                    600: {
+                        items: 2,
+                    },
+                    1000: {
+                        items: 3,
+                    }
+                }
+            });
+        });
 	</script>
 @endsection
